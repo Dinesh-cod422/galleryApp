7c96e960318ed8883765667fdffd771746b1299c
Dinesh-cod422 <dineshkumarmdk01@gmail.com>
2026-06-16 20:36:16 +0530

    Add upload button to MobileNavbar

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -41,7 +41,17 @@ export default function MobileNavbar() {
         </svg>
       )
     },
-
+    {
+      name: "Upload",
+      href: "/upload",
+      isAction: true,
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <line x1="12" y1="5" x2="12" y2="19"></line>
+          <line x1="5" y1="12" x2="19" y2="12"></line>
+        </svg>
+      )
+    },
     {
       name: "Wishlist",
       href: "/wishlist",
